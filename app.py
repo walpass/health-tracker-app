bd85c711c14bd8e945fa1a7ac1220ddf044e254a
WALPASS <support@walpass.jp>
2025-06-04 23:20:14 +0900

    Increase password column length in User model to 255

--- app.py
+++ app.py
@@ -33,7 +33,7 @@ class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(20), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(60), nullable=False)
+    password = db.Column(db.String(255), nullable=False)
     records = db.relationship('HealthRecord', backref='author', lazy=True)
 
     def __repr__(self):
@@ -255,4 +255,4 @@ if __name__ == '__main__':
     # Renderではビルドコマンドで一度だけ実行するため、ここではコメントアウトしておく
     # with app.app_context():
     #     db.create_all()
-     pass 
+    pass # このままでOK
